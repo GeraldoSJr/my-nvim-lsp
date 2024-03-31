b13d447f39ba643b5cc39f60413dcf50bf6511ab
Geraldo157 <gsmecanica76@gmail.com>
2024-03-31 13:36:26 -0300

    sending

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    'uloco/bluloco.nvim',
+    lazy = false,
+    priority = 1000,
+    dependencies = { 'rktjmp/lush.nvim' },
+    config = function()
+      require("bluloco").setup({
+        style = "dark",
+        transparent = false,
+        italics = false,
+        terminal = vim.fn.has("gui_running") == 1,
+        guicursor   = true,
+      })
+
+      vim.opt.termguicolors = true
+      vim.cmd('colorscheme bluloco')
+    end,
+  },
+}
---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+
+      local lspconfig = require("lspconfig")
+      lspconfig.tsserver.setup({})
+      lspconfig.gopls.setup({})
+      lspconfig.intelephense.setup({})
+      lspconfig.pyright.setup({})
+      lspconfig.rust_analyzer.setup({})
+      lspconfig.csharp_ls.setup({})
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+}
