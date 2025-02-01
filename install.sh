f509330ae2d8cdf3bee214d19a50ee875014dd43
Geraldo <gsmecanica76@gmail.com>
2025-02-01 11:37:21 -0300

    creating setup script

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/binbash
+
+git clone https://github.com/ThePrimeagen/vim-apm.git /home/$USER/personal/vim-apm
+git clone https://github.com/ThePrimeagen/vim-with-me.git /home/$USER/personal/vim-with-me
